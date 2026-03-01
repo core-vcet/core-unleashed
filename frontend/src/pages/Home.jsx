be2f3abc2c763d327a4e9c8b390e97f99f1e683f
kshku <1211shree@gmail.com>
2026-03-02 00:01:18 +0530

    upadte the brochure link

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -95,7 +95,7 @@ const Home = () => {
           <button
             className="w-full sm:w-auto bg-white text-black px-6 sm:px-10 md:px-14 lg:px-16 py-2.5 sm:py-3 md:py-4 lg:py-5 rounded-lg text-sm sm:text-base md:text-xl lg:text-2xl hover:bg-gray-100 transition-colors"
             style={{ fontFamily: "Inter", fontWeight: 700 }}
-            onClick={() => window.open("https://drive.google.com/file/d/1_8RhqN2ud7jOgF6RCR6AhRcBuWhIibHF/view?usp=sharing", "_blank", "noopener,noreferrer")}
+            onClick={() => window.open("https://drive.google.com/file/d/1ezkI6i7L3XjDwt-Vl_ysG9j7-ehV-IbH/view?usp=sharing", "_blank", "noopener,noreferrer")}
           >
             Download Brochure
           </button>
